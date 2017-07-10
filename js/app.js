1909c65b122e8496a2b666e888921a521bcfb084
Dave Brown <daveb.za@gmail.com>
2017-07-10 11:05:16 +0800

    FEAT: KO added nicknames as ul

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.nicks = ko.observableArray(["Bitmap", "Stupid", "Lumpy"]);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount()+1);
