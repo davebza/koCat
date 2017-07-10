365d7881f67b9ddba05b345f20c69f2d91bf9e95
Dave Brown <daveb.za@gmail.com>
2017-07-10 11:28:21 +0800

    FEAT: used KO with databinding method

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 var ViewModel = function() {
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount()+1);
+    };
+
+}
+
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.nicks = ko.observableArray(["Bitmap", "Stupid", "Lumpy"]);
-
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount()+1);
-
-    };
 
     this.level = ko.computed(function(){
         if(this.clickCount() < 10){
@@ -15,8 +20,9 @@ var ViewModel = function() {
         }else if (this.clickCount() < 20) {
             return("IttyBittyKitty");
         };
-    }, this);
+    }, this);    
 
 }
+
 
 ko.applyBindings(new ViewModel());
